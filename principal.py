41e8ffa4238c811531ac07b3350dcfa61a2da991
Andrew de Andrade <andrewalmeida2110@gmail.com>
2025-09-20 18:48:26 -0300

    :bug: fix: corrige a lógica da função tipo3

--- principal.py
+++ principal.py
@@ -9,6 +9,8 @@ def processarLinha(linha):
     coluna2.append(substring2)
 
 def lerArquivo():
+    coluna1.clear() #Para quando o usuário insere outra gramática sem reiniciar o programa.
+    coluna2.clear()
     with open("arquivo.txt", "r") as arqGram:
         for linha in arqGram:
             linha = linha.rstrip() #Remove caracteres do lado direito da string (Ex: \n)
@@ -45,24 +47,27 @@ def tipo2(coluna1,coluna2):
     
 def tipo3(coluna1, coluna2):
     for i in range(len(coluna1)):
-        #Caso 1:
-        #Verifica se possui apenas 1 elemento não terminal do lado direito da seta.
-        if(sum(1 for c in coluna2[i] if c.isupper()) == 1):
-            tamColuna2 = (len(coluna2[i])) - 1
-            #Verifica se esse elemento não terminal está na última posição da string.
-            if((coluna2[i][tamColuna2]).isupper()):
-                return True
-        #Caso 2:
-        #Se o lado direito da seta não possuir um elemento não terminal, retorna true.
-        if not any(c.isupper() for c in coluna2[i]):
-            return True
-        #Caso 3:
         #Verifica se o lado esquerdo da seta possui apenas um elemento não terminal.
-        if (coluna1[i] == 1 and (coluna1[i][0]).isupper()):
-            #Verifica se o lado direito da seta possui o símbolo de vazio (seja ele maiúsculo ou minúsculo).
-            if(coluna2[i][0] == 'E' or coluna2[i][0] == 'e'):
-                return True
-    return False
+        if not (len(coluna1[i]) == 1 and coluna1[i][0].isupper()):
+            return False
+        #Caso 1 (A -> aB)
+        if sum(1 for c in coluna2[i] if c.isupper()) == 1:
+            tam = len(coluna2[i])
+            if coluna2[i][tam - 1].isupper():  #Checa se o elemento não terminal está na última posição.
+                continue 
+            else:
+                return False #Caso o não terminal NÃO esteja na última posição do lado direito da seta, retorna falso.
+        #Caso 2 (A -> a)
+        elif not any(c.isupper() for c in coluna2[i]):
+            continue
+        #Caso 3 (A -> e)
+        elif coluna2[i] == 'E' or coluna2[i] == 'e':
+            continue
+        else:
+            #Caso não atenda nenhum dos casos acima, retorna falso, dizendo que não é uma gramática do tipo 3.
+            return False
+    #Como o programa chegou até aqui sem retornar falso para nenhuma das regras, então é uma gramática do tipo 3.
+    return True
 
 def tipo_gramatica(coluna1, coluna2):
 
