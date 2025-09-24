868e9bcaa8c6fa7ff312fc5b71a1b13a134daafd
EnzoMagaldi <166729058+EnzoMagaldi@users.noreply.github.com>
2025-09-23 21:16:01 -0300

    🧹cleanup: Dispensando comentários

--- principal.py
+++ principal.py
@@ -2,18 +2,18 @@ coluna1 = []
 coluna2 = []
 
 def processarLinha(linha):
-    pos = linha.find(' ') #Procura pelo espaço dentro da string.
-    substring1 = linha[0:pos] #Pega a primeira parte (até a seta) da string linha.
-    substring2 = linha[pos+1:len(linha)] #Pega a segunda parte (depois da seta) da string linha.
+    pos = linha.find(' ') 
+    substring1 = linha[0:pos]
+    substring2 = linha[pos+1:len(linha)]
     coluna1.append(substring1)
     coluna2.append(substring2)
 
 def lerArquivo():
-    coluna1.clear() #Para quando o usuário insere outra gramática sem reiniciar o programa.
+    coluna1.clear() 
     coluna2.clear()
     with open("arquivo.txt", "r") as arqGram:
         for linha in arqGram:
-            linha = linha.rstrip() #Remove caracteres do lado direito da string (Ex: \n)
+            linha = linha.rstrip()
             processarLinha(linha)
     print("Arquivo lido com sucesso!")
 
@@ -21,45 +21,36 @@ def tipo1(coluna1, coluna2):
     for i in range(len(coluna1)):
         if len(coluna1[i]) > len(coluna2[i]):
             return False
-        if coluna2[i] == 'E' and coluna1[i] != 'S':
-            return False
+        if coluna2[i] == 'E':
+            if coluna1[i] != 'S':
+                return False
+            for j in range(len(coluna1)):
+                if 'S' in coluna2[j]:
+                    return False
+            continue
     return True
 
-def tipo2(coluna1,coluna2):
+def tipo2(coluna1):
     for i in range(len(coluna1)):
-        if len(coluna1[i]) > 2:
+        for j in range(len(coluna1[i])):
+            if coluna1[i][j].islower():
+                return False
+    return True
+
+def tipo3(coluna1,coluna2):
+    for i in range(len(coluna1)):
+        if len(coluna1[i]) > 1:
             return False
         if len(coluna2[i]) > 2:
-            return False
+            return False 
         if len(coluna2[i]) == 2:
             if coluna2[i][1].islower():
                 return False
+        if len(coluna2[i]) == 1:
+            if coluna2[i].isupper() and coluna2[i] != 'E':
+                return False
     return True
     
-def tipo3(coluna1, coluna2):
-    for i in range(len(coluna1)):
-        #Verifica se o lado esquerdo da seta possui apenas um elemento não terminal.
-        if not (len(coluna1[i]) == 1 and coluna1[i][0].isupper()):
-            return False
-        #Caso 1 (A -> aB)
-        if sum(1 for c in coluna2[i] if c.isupper()) == 1:
-            tam = len(coluna2[i])
-            if coluna2[i][tam - 1].isupper():  #Checa se o elemento não terminal está na última posição.
-                continue 
-            else:
-                return False #Caso o não terminal NÃO esteja na última posição do lado direito da seta, retorna falso.
-        #Caso 2 (A -> a)
-        elif not any(c.isupper() for c in coluna2[i]):
-            continue
-        #Caso 3 (A -> e)
-        elif coluna2[i] == 'E' or coluna2[i] == 'e':
-            continue
-        else:
-            #Caso não atenda nenhum dos casos acima, retorna falso, dizendo que não é uma gramática do tipo 3.
-            return False
-    #Como o programa chegou até aqui sem retornar falso para nenhuma das regras, então é uma gramática do tipo 3.
-    return True
-
 def tipo_gramatica(coluna1, coluna2):
 
     tipo_gramatica = 0
